2fdae6db780ad22b110d7bd5cecfe316ee91d8de
Yann <yannci2002@gmail.com>
2018-07-25 23:59:13 +0200

    Visual Updates

--- src/main/java/de/unhandledexceptions/codersclash/bot/listeners/MentionListener.java
+++ src/main/java/de/unhandledexceptions/codersclash/bot/listeners/MentionListener.java
@@ -54,8 +54,8 @@ public class MentionListener extends ListenerAdapter {
                     .addField("Members", Long.toString(members), true)
                     .addField("Current Uptime", this.getUptime(), true)
                     .addField("Source Code", "[GitHub](https://github.com/JohnnyJayJay/codersclash-unhandledexceptions-bot)", false)
-                    .addField("Need Help?", "[Our Developement Server](https://discord.gg/gR5ukYK)", false)
-                    .addField("Help translating me!", "[Crowdin]()", true)
+                    .addField("Need Help?", "[Our Developement Server](https://discord.gg/gR5ukYK)", true)
+                    .addField("Help translating me!", "[Crowdin]()", false)
                     .setColor(event.getGuild().getSelfMember().getColor());
             Messages.sendMessage(event.getChannel(), Messages.Type.NO_TYPE, "Introducing... me!", "Hi!", false, builder).queue();
             //Messages.sendMessage(event.getChannel(), Messages.Type.NO_TYPE, "Introducing... me!", "Hi!", false, builder).queue(this::reactionsAdd);
